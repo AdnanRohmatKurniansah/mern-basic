c57324169c29c675817d1445a1b05e806c860d61
AdnanRohmatKurniansah <atnandimas@gmail.com>
2023-08-17 18:37:55 +0700

    add show and remove blog

--- backend/src/controllers/blogController.js
+++ backend/src/controllers/blogController.js
@@ -1,5 +1,20 @@
 const {validationResult} = require('express-validator')
 const Blog = require('../models/blog')
+const path = require('path')
+const fs = require('fs')
+
+exports.index = (req, res, next) => {
+    Blog.find()
+    .then(result => {
+        res.status(200).json({
+            message: 'Berhasil memanggil data',
+            data: result
+        })
+    })
+    .catch(err => {
+        next(err)
+    })
+}
 
 exports.create = (req, res, next) => {
     const errors = validationResult(req)
@@ -42,4 +57,96 @@ exports.create = (req, res, next) => {
     .catch(err => {
         console.log('err :', err)
     })
+}
+
+exports.show = (req, res, next) => {
+    Blog.findById(req.params.blogId)
+    .then(result => {
+        if (!result) {
+            const error = new Error('Blog tdk ditemukan')
+            error.errorStatus = 404
+            throw error
+        }
+        res.status(200).json({
+            message: 'Berhasil memanggil data',
+            data: result    
+        })
+    })
+    .catch(err => {
+        next(err)
+    })
+}
+
+exports.update = (req, res, next) => {
+    const errors = validationResult(req)
+    
+    if (!errors.isEmpty()) {
+        const err = new Error('Invalid value')
+        err.errorStatus = 400
+        err.data = errors.array()
+        throw err
+    }
+
+    if (!req.file) {
+        const err = new Error('Image harus diupload')
+        err.errorStatus = 422
+        throw err
+    }
+
+    const title = req.body.title
+    const body = req.body.body
+    const image = req.file.path
+
+    Blog.findById(req.params.blogId)
+    .then(blog => {
+        if (!blog) {
+            const err = new Error('Blog tdk ditemukan')
+            err.errorStatus = 404
+            throw error
+        }
+
+        blog.title = title
+        blog.body = body
+        blog.image = image
+
+        return blog.save()
+    })
+    .then(result => {
+        res.status(200).json({
+            message: 'Berhasil mengupdate blog',
+            data: result    
+        })
+    })
+    .catch(err => {
+        next(err)
+    })
+
+}
+
+exports.destroy = (req, res, next) => {
+    Blog.findById(req.params.blogId)
+    .then(blog => {
+        if (!blog) {
+            const error = new Error('Blog tdk ditemukan')
+            error.errorStatus = 404
+            throw error
+        }
+        
+        removeImage(blog.image)
+        return Blog.findByIdAndRemove(req.params.blogId)
+    })
+    .then(result => {
+        res.status(200).json({
+            message: 'Berhasil menghapus blog',
+            data: result   
+        })
+    })
+    .catch(err => {
+        next(err)
+    })
+}
+
+const removeImage = (filePath) => {
+    filePath = path.join(__dirname, '../..', filePath)
+    fs.unlink(filePath, err => console.log(err))
 }
--- backend/src/routes/blog.js
+++ backend/src/routes/blog.js
@@ -3,9 +3,20 @@ const router = express.Router()
 const {body} = require('express-validator')
 const blogController = require('../controllers/blogController')
 
+router.get('/index', blogController.index)
+
 router.post('/create', 
     [body('title').isLength({min: 5}).withMessage('title tdk sesuai')], 
     [body('body').isLength({min: 5}).withMessage('body tdk sesuai')], 
     blogController.create)
+
+router.get('/:blogId', blogController.show)
+
+router.put('/edit/:blogId', 
+    [body('title').isLength({min: 5}).withMessage('title tdk sesuai')], 
+    [body('body').isLength({min: 5}).withMessage('body tdk sesuai')], 
+    blogController.update)
+
+router.delete('/:blogId', blogController.destroy)
 
 module.exports = router
